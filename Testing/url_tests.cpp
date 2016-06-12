6c04dda19fdf23a9cd28d611e7e1fa3acf2d63ee
caetanus <marcelo@fiveti.com>
2016-06-12 16:43:57 -0300

    added some more tests.

--- Testing/url_tests.cpp
+++ Testing/url_tests.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "utils/url.h"
 
 
@@ -47,7 +48,7 @@ TEST(url_test_case, unquote_plus_test_unregular)
 TEST(url_test_case, url_decode_map_empty)
 {
     auto result{Wizrd::URL::decodeMap("")};
-    std::map<std::string, std::string> expect;
+    Wizrd::paramsMap expect; // empty map
 
     EXPECT_EQ(result, expect);
 }
@@ -55,16 +56,16 @@ TEST(url_test_case, url_decode_map_empty)
 TEST(url_test_case, url_decode_map_common)
 {
     auto result{Wizrd::URL::decodeMap("foo=bar&ba+=+baz+")};
-    std::map<std::string, std::string> expect{{"foo", "bar"},
-                                              {"ba ", " baz "}};
+    Wizrd::paramsMap expect{{"foo", "bar"},
+                            {"ba ", " baz "}};
     EXPECT_EQ(result, expect);
 }
 
 TEST(url_test_case, url_decode_map_with_some_key_with_no_value)
 {
     auto result{Wizrd::URL::decodeMap("foo&ba+=+baz+")};
-    std::map<std::string, std::string> expect{{"foo", ""},
-                                              {"ba ", " baz "}};
+    Wizrd::paramsMap expect{{"foo", ""},
+                            {"ba ", " baz "}};
     EXPECT_EQ(result, expect);
 }
 
@@ -72,14 +73,14 @@ TEST(url_test_case, url_decode_empty)
 {
 
     auto result{Wizrd::URL::decode("")};
-    std::vector<std::vector<std::string>> expect;
+    Wizrd::params expect;
     EXPECT_EQ(result, expect);
 }
 TEST(url_test_case, url_decode_item_empty)
 {
 
     auto result{Wizrd::URL::decode("foo=&bar=+")};
-    std::vector<std::vector<std::string>> expect{{"foo", ""},{"bar", " "}};
+    Wizrd::params expect{{"foo", ""}, {"bar", " "}};
     EXPECT_EQ(result, expect);
 }
 
@@ -88,7 +89,7 @@ TEST(url_test_case, url_decode_common)
 {
 
     auto result{Wizrd::URL::decode("foo=bar&ba+=+baz+")};
-    std::vector<std::vector<std::string>> expect{{"foo", "bar"}, {"ba ", " baz "}};
+    Wizrd::params expect{{"foo", "bar"}, {"ba ", " baz "}};
     EXPECT_EQ(result, expect);
 }
 
@@ -96,7 +97,40 @@ TEST(url_test_case, url_decode_with_some_keys_with_no_value)
 {
 
     auto result{Wizrd::URL::decode("foo&ba+=+baz+")};
-    std::vector<std::vector<std::string>> expect{{"foo"}, {"ba ", " baz "}};
+    Wizrd::params expect{{"foo"}, {"ba ", " baz "}};
     EXPECT_EQ(result, expect);
+}
+
+TEST(url_test_case, url_encode_common)
+{
+    Wizrd::params params{{"foo", "bar"}, {"  foo  ", "ba@"}};
+    Wizrd::paramsMap map{{"foo", "bar"}, {"  foo  ", "ba@"}};
+    std::string expect{"foo=bar&++foo++=ba%40"};
+    auto result1{Wizrd::URL::encode(params)};
+    auto result2{Wizrd::URL::encode(map)};
+    EXPECT_EQ(result1, expect);
+    // EXPECT_EQ(result2, expect);
+    // not testing that because of the order,
+    // try reencoding and see if the result is equal
+    EXPECT_EQ(Wizrd::URL::decodeMap(result2), map);
+}
+
+TEST(url_test_case, url_encode_empty)
+{
+    Wizrd::params params;
+    Wizrd::paramsMap map;
+    std::string expect{""};
+    auto result1{Wizrd::URL::encode(params)};
+    auto result2{Wizrd::URL::encode(map)};
+    EXPECT_EQ(result1, expect);
+    EXPECT_EQ(result2, expect);
+
+}
+
+TEST(url_test_case, url_encode_invalid_number_of_parameters_should_throw_URLEncodeError)
+{
+    //creating a parameter vector with itens with key, value and one item having 3 items (that is invalid)
+    Wizrd::params params{{"foo", "bar"}, {"  foo  "}, {"a", "b", "c"}};
+    EXPECT_THROW(Wizrd::URL::encode(params), Wizrd::URLEncodeError);
 }
 
